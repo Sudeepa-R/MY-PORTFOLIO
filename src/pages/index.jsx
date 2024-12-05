05c8e2592d1e475c9dcd7c77bb0c300e20b68b07
Sudeepa-R <sudeepar084@gmail.com>
2024-12-05 21:55:51 +0530

    shower bg added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './index.scss'
 import NavBar from './navbar/NavBar'
+import MeteorDemo from '../design-ui/homePageBackground'
 
 export default class HomePage extends Component {
   render() {
     return (
       <div className='homePage'>
         <NavBar/>
+        
       </div>
     )
   }
