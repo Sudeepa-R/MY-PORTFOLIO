d46cc505ea12ca50a7e646c529662277e026db1e
Sudeepa-R <sudeepar084@gmail.com>
2024-12-05 22:53:54 +0530

    bg animation added

---
+++ src/design-ui/HomePageAnimation.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { useScroll, useTransform } from "framer-motion";
+import React from "react";
+import { GoogleGeminiEffect } from "../components/ui/google-gemini-effect";
+import "./design.scss";
+import NavBar from "../pages/navbar/NavBar";
+
+export default function GoogleGeminiEffectDemo() {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
+  return (
+    <div>
+      <div className="navBg">
+        <NavBar />
+      </div>
+      <div
+        className="h-[400vh]   dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip homePage"
+        ref={ref}
+      >
+        <GoogleGeminiEffect
+          style={{ outerHeight: "60px" }}
+          pathLengths={[
+            pathLengthFirst,
+            pathLengthSecond,
+            pathLengthThird,
+            pathLengthFourth,
+            pathLengthFifth,
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
